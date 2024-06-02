8d78763413eabdfa857e9b0458d630382984d0fc
ndunnett <nathandunnett@me.com>
2024-06-02 09:07:41 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,24 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let mut parts = input.split_whitespace();
 
-    let mut parts = input.split_whitespace();
+        let result = match parts.next() {
+            Some("cd") => unimplemented!(),
+            Some("echo") => unimplemented!(),
+            Some(keyword) => Err(format!("{keyword}: command not found")),
+            None => Ok(()),
+        };
 
-    match parts.next() {
-        Some("cd") => unimplemented!(),
-        Some("echo") => unimplemented!(),
-        Some(keyword) => eprintln!("{keyword}: command not found"),
-        None => (),
+        if let Err(e) = result {
+            eprintln!("{}", e);
+        }
     }
 }
