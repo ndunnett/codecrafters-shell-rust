408dd6111ec039a452b365f1f999d5d5000f4cad
ndunnett <nathandunnett@me.com>
2024-06-02 09:12:40 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let stdin = io::stdin();
         let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
+        io::stdin().read_line(&mut input).unwrap();
         let mut parts = input.split_whitespace();
 
         let result = match parts.next() {
             Some("cd") => unimplemented!(),
             Some("echo") => unimplemented!(),
+            Some("exit") => exit(
+                parts
+                    .next()
+                    .map(|arg| arg.parse::<i32>().unwrap_or(0))
+                    .unwrap_or(0),
+            ),
             Some(keyword) => Err(format!("{keyword}: command not found")),
             None => Ok(()),
         };
